e3f49f5d9d72a81ccc67fa21bacfa1a86a50a6d8
mercycermy <mercycermy5@gmail.com>
2025-06-03 15:34:36 -0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/utopia/', // 👈 important!
+  base: '/utopia', // 👈 important!
   plugins: [react()],
 });
